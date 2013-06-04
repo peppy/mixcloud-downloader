c5a6dd4815b43f45a2ddd107dacb61e0907390e8
Dean Herbert <pe@ppy.sh>
2013-06-04 23:25:46 +0900

    add fork graphic

--- mixcloud.php
+++ mixcloud.php
@@ -6,7 +6,7 @@ $url = $_REQUEST['url'];
 
 if ($url)
 {
-  if (strpos($url, 'http://www.mixcloud.com/') !== 0)
+	if (strpos($url, 'http://www.mixcloud.com/') !== 0)
 		exit();
 
 	$content = file_get_contents($url);
@@ -89,4 +89,6 @@ if ($url)
 			});
 		</script>
 	</body>
+
+	<a href="https://github.com/peppy/mixcloud-downloader"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
 </html>
